2591a44bf9f6bba3538ebdf326d8fd2faea1133a
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-03 00:05:47 -0400

    Add http respond code

--- url-handler/app.py
+++ url-handler/app.py
@@ -1,6 +1,6 @@
 from redis import Redis
 from cassandra.cluster import Cluster
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, abort
 
 
 app = Flask(__name__)
@@ -17,9 +17,13 @@ session = cluster.connect('urlshortener')
 def save_long_url(long_url):
     short_url = request.args.get('short')
     long_url = request.args.get('long')
+
+    if not short_url or not long_url:
+        return 'bad request', 400
+
     master.set(short_url, long_url)
     session.execute("INSERT INTO urls (short_url, long_url) VALUES (%s, %s)", (short_url, long_url))
-    return 'OK'
+    return '', 200
 
 
 @app.route('/<short_url>', methods=['GET'])
@@ -31,8 +35,11 @@ def redirect_url(short_url):
         long_url = rows.long_url
         master.set(short_url, long_url)
 
+        if not long_url:
+            abort(404, description='page not found')
+
     return redirect(long_url, code=307)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
