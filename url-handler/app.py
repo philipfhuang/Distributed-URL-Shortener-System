bd5c28fd126e329385f8ee8e08b4dd43472bed38
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-02 21:22:17 -0400

    Add master and salve for redis

--- url-handler/app.py
+++ url-handler/app.py
@@ -1,11 +1,12 @@
-import redis
+from redis import Redis
 from cassandra.cluster import Cluster
 from flask import Flask, request, redirect
 
 
 app = Flask(__name__)
-# TODO: Need to change the IP address to the IP address of the Redis container
-r = redis.Redis(host='redis-primary', port=6379)
+master = Redis(host='redis-primary', port=6379)
+slave = Redis(host='redis-replica', port=6379)
+
 # TODO: Need to change the IP address to the IP address of the Cassandra container
 cluster = Cluster(['127.0.0.1'])
 # TODO: Need to change the keyspace to the keyspace of the Cassandra container
@@ -16,19 +17,19 @@ session = cluster.connect('urlshortener')
 def save_long_url(long_url):
     short_url = request.args.get('short')
     long_url = request.args.get('long')
-    r.set(short_url, long_url)
+    master.set(short_url, long_url)
     session.execute("INSERT INTO urls (short_url, long_url) VALUES (%s, %s)", (short_url, long_url))
     return 'OK'
 
 
 @app.route('/<short_url>', methods=['GET'])
 def redirect_url(short_url):
-    long_url = r.get(short_url)
+    long_url = slave.get(short_url)
 
     if not long_url:
         rows = session.execute("SELECT long_url FROM urls WHERE short_url = %s", (short_url,))
         long_url = rows.long_url
-        r.set(short_url, long_url)
+        master.set(short_url, long_url)
 
     return redirect(long_url, code=307)
 
