d00cfdc2a04b466ddc6323865d90ae3b980981b4
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-06 22:43:58 -0500

    Add script for launching v2

--- fastautostart.sh
+++ fastautostart.sh
@@ -1,4 +1,5 @@
 docker service rm $(docker service ls -q)
-docker stack deploy -c url-handler.yml url
+docker stack deploy -c fast-url-handler.yml url
 docker service scale url_redis-replica=2
+docker service scale url_worker=3
 docker service scale url_url-handler=3
