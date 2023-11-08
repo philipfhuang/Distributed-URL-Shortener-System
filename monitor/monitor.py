c99f30df00864bf6aa41e9d64f652f11087a9c87
Feiran Huang <philipp.huang@mail.utoronto.ca>
2023-11-07 19:24:05 -0500

    fixed bugs

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,36 +1,29 @@
 from flask import Flask, render_template, jsonify
+import json
 import subprocess
 
 app = Flask(__name__)
 
-def convert_to_json(output, node):
-    lines = output.split('\n')
-
-    if len(lines) < 2:
-        return []
-
-    header = lines[0]
-    columns = header.split()
-
-    docker_info = []
-
-    for line in lines[1:]:
-        datas = line.split()
-        result_dict["NODE ID"] = node
-        result_dict = {columns[i]: datas[i] for i in range(len(columns))}
-        docker_info.append(result_dict)
-
-    return docker_info
-
 @app.route('/get_docker_nodes')
 def get_docker_nodes():
     nodes_info = []
-    ips = ["10.128.1.42", "10.128.2.42", "10.128.3.42", "10.182.4.42"]
+    ips = ["10.128.1.42", "10.128.2.42", "10.128.3.42", "10.128.4.42"]
     for ip in ips:
-        output = subprocess.check_output(f"ssh {ip} 'docker ps -a'", shell=True, stderr=subprocess.STDOUT, text=True)
-        node = subprocess.check_output(f"ssh {ip} 'hostname'", shell=True, stderr=subprocess.STDOUT, text=True)
-        node_info = convert_to_json(output, node)
-        nodes_info.extend(node_info)
+        try:
+            output = subprocess.check_output(f"ssh {ip} 'docker ps -a --format json --no-trunc'", shell=True, stderr=subprocess.STDOUT, text=True, timeout=5)
+            node = subprocess.check_output(f"ssh {ip} 'hostname'", shell=True, stderr=subprocess.STDOUT, text=True, timeout=5)
+        except subprocess.TimeoutExpired:
+            return jsonify([{
+                "Node": ip,
+                "Info": "TIMEOUT",
+            }])
+        outputs = output.split('\n')
+        node_info = [json.loads(output) for output in outputs if output != '']
+        node_infos = {
+            "Node": node.strip(),
+            "Info": node_info,
+        }
+        nodes_info.append(node_infos)
     return jsonify(nodes_info)
 
 
