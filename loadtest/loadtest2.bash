ed862c0c20a3d9167d68ed0025a995c98a11e4fa
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-10 11:46:19 -0500

    Load test naming err

--- loadtest/loadtest2.bash
+++ loadtest/loadtest2.bash
@@ -1,9 +1,9 @@
 #!/bin/bash 
-javac loadtest.java
+javac LoadTest.java
 echo $SECONDS 
-java loadtest 127.0.0.1 8081 11 GET 1000 &
-java loadtest 127.0.0.1 8081 12 GET 1000 &
-java loadtest 127.0.0.1 8081 100 GET 1000 &
-java loadtest 127.0.0.1 8081 101 GET 1000 &
+java LoadTest 127.0.0.1 8081 11 GET 1000 &
+java LoadTest 127.0.0.1 8081 12 GET 1000 &
+java LoadTest 127.0.0.1 8081 100 GET 1000 &
+java LoadTest 127.0.0.1 8081 101 GET 1000 &
 wait $(jobs -p) 
 echo $SECONDS
