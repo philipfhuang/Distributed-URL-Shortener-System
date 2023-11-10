b40aa2e67ca3c8c76c558b8c8318f426a1ffc438
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-10 14:16:38 -0500

    Add replicas

--- fastautostart.sh
+++ fastautostart.sh
@@ -1,5 +1,2 @@
 docker service rm $(docker service ls -q)
 docker stack deploy -c fast-url-handler.yml url
-docker service scale url_redis-replica=2
-docker service scale url_worker=3
-docker service scale url_fast-handler=3
