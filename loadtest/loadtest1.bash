d74d65f8e05c5229eb966c77845af6aaa325c693
Jingwen Shi <jingwensteven.shi@gmail.com>
2023-11-10 11:47:25 -0500

    Change load test of 1000 PUT request to 100.

--- loadtest/loadtest1.bash
+++ loadtest/loadtest1.bash
@@ -1,9 +1,9 @@
 #!/bin/bash 
 javac LoadTest.java
 echo $SECONDS 
-java LoadTest 127.0.0.1 8081 11 PUT 1000 &
-java LoadTest 127.0.0.1 8081 12 PUT 1000 &
-java LoadTest 127.0.0.1 8081 13 PUT 1000 &
-java LoadTest 127.0.0.1 8081 14 PUT 1000 &
+java LoadTest 127.0.0.1 8081 11 PUT 100 &
+java LoadTest 127.0.0.1 8081 12 PUT 100 &
+java LoadTest 127.0.0.1 8081 13 PUT 100 &
+java LoadTest 127.0.0.1 8081 14 PUT 100 &
 wait $(jobs -p) 
 echo $SECONDS
