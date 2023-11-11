9b4b8a5cb3989109519600e58d3ff48df14275d1
zhuxuank <xk.zhu@mail.utoronto.ca>
2023-11-11 00:36:27 +0000

    logger fixed

--- fast-url-hanlder/logger/logger.py
+++ fast-url-hanlder/logger/logger.py
@@ -3,23 +3,15 @@ import os
 
 master = redis.Redis(host='redis-primary', port=6379)
 
-home_directory = os.path.expanduser("~")
-
-relative_path = "data"
-
-file_name = "appendonlylog"
-
-file_path = os.path.join(home_directory, relative_path, file_name)
-
-try:
-    log = open("appendonlylog", "a+")
-except Exception:
-    print("could not open log file")
-    exit(1)
-
+#try:
+#    log = open("appendonlylog", "a+")
+#except Exception:
+#    print("could not open log file")
 
 stream_name = 'log_stream'
 group_name = 'log_group'
+data_dir = '/app/data'
+os.makedirs(data_dir, exist_ok=True)
 
 try:
     master.xgroup_create(stream_name, group_name, id='0', mkstream=True)
@@ -35,19 +27,19 @@ def process_messages():
     while True:
         try:
             messages = master.xreadgroup(group_name, 'log_consumer', {stream_name: '>'}, count=100, block=1000)
-            for message in messages:
-                msg_id, data = message
-                short_url = data['short_url']
-                long_url = data['long_url']
+            with open(os.path.join(data_dir, 'appendonlylog'), 'a+') as log:    
+                for stream, stream_message in messages:
+                    for message in stream_message:
+                        msg_id, data = message
+                        short_url = data['short_url']
+                        long_url = data['long_url']
 
-                log.write(f"inserted pair short: {short_url}    long: {long_url}")
-                log.flush()
+                        log.write(f"inserted pair short: {short_url}    long: {long_url}")
+                        log.flush()
 
-                master.xack(stream_name, group_name, msg_id)
-        except Exception:
-            pass
-        finally:
-            log.close()
+                        master.xack(stream_name, group_name, msg_id)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
