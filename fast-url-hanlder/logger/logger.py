b241528cffbf0995a570759905dbe5a459e5e246
phili <philipp.huang@mail.utoronto.ca>
2023-11-11 01:13:41 -0500

    fixed logger cant write bug

--- fast-url-hanlder/logger/logger.py
+++ fast-url-hanlder/logger/logger.py
@@ -1,12 +1,12 @@
 import redis
 import os
+from datetime import datetime
 
 master = redis.Redis(host='redis-primary', port=6379)
 
 stream_name = 'log_stream'
 group_name = 'log_group'
-data_dir = '/app/data'
-#os.makedirs(data_dir, exist_ok=True)
+data_dir = '~/logs'
 
 try:
     master.xgroup_create(stream_name, group_name, id='0', mkstream=True)
@@ -14,24 +14,22 @@ except redis.exceptions.ResponseError as e:
     if "BUSYGROUP Consumer Group name already exists" in str(e):
         print("Consumer group already exists.")
     else:
-        raise e
+        print(e)
 
 
 def process_messages():
-    print('running')
     while True:
         try:
             messages = master.xreadgroup(group_name, 'log_consumer', {stream_name: '>'}, count=100, block=1000)
             with open(os.path.join(data_dir, "logfile"), 'a+') as log:
-                log.write("writing... ")
                 for stream, stream_message in messages:
                     for message in stream_message:
                         msg_id, data = message
-                        short_url = data['short_url']
-                        long_url = data['long_url']
 
-                        log.write(f"inserted pair short: {short_url}    long: {long_url}")
-                        log.flush()
+                        short_url = data[b'short_url'].decode('utf-8')
+                        long_url = data[b'long_url'].decode('utf-8')
+
+                        log.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] short: {short_url},    long: {long_url}')
 
                         master.xack(stream_name, group_name, msg_id)
         except Exception as e:
