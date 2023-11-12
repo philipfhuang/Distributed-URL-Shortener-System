afa5713a45d53f18e85f9d447db374bc8ab9691e
phili <philipp.huang@mail.utoronto.ca>
2023-11-12 17:38:59 -0500

    fixed typo

--- start_docker_swarm.sh
+++ start_docker_swarm.sh
@@ -6,7 +6,7 @@ if [ "$#" == "0" ]; then
 	exit 1
 fi
 
-COMMAND = ssh student@$1 docker swarm leave --force > /dev/null 2>&1; docker swarm init --advertise-addr $1 | grep "docker swarm" | head -n 1
+COMMAND=ssh student@$1 docker swarm leave --force > /dev/null 2>&1; docker swarm init --advertise-addr $1 | grep "docker swarm" | head -n 1
 shift
 while (( "$#" )); do
     ssh student@$1 "docker swarm leave --force > /dev/null; $COMMAND"
